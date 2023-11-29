d1a553b2412c9c11b930ccece10220972845ea31
Mostafa Mohamed <mostafa.mohamed.1850@gmail.com>
2023-11-29 16:47:18 +0200

    remove conosle log

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 document.getElementsByClassName('form-rating')[0].addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log('test');
     let selectedRate = document.querySelector('input[name="rating"]:checked').value;
     document.getElementById("selected-rate").innerHTML = selectedRate;
     document.getElementsByClassName('rating-container')[0].style.display = "none";
