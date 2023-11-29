3ae160b380040ef0801899f93f9388fc568bccb7
Mostafa Mohamed <mostafa.mohamed.1850@gmail.com>
2023-11-29 16:36:45 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+document.getElementsByClassName('form-rating')[0].addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('test');
+    let selectedRate = document.querySelector('input[name="rating"]:checked').value;
+    document.getElementById("selected-rate").innerHTML = selectedRate;
+    document.getElementsByClassName('rating-container')[0].style.display = "none";
+    document.getElementsByClassName('thank-you-container')[0].style.display = "grid";
+    return false;
+})
+
+
+
